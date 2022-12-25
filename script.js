5b6baf975c26ce645fc4729938e47ff32ef5f3ea
wassim zisous <sissimodidoud@gmail.com>
2022-12-26 00:32:35 +0100

    gettin the values of the cards

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let deck=[]
 
 creatTHCards()
 shufelTheCards()
-console.log(deck)
+
 function creatTHCards(){
     let value=['A','2','3','4','5','6','7','8','9','10','J','Q','K']
     let type=['D','H','S','C']
@@ -24,6 +24,27 @@ function shufelTheCards(){
         deck[i]=deck[random]
         deck[random]=place
     }
+}
+
+
+function getValue(card){
+    
+    let split=card.split('-')
+    let value=split[0]
+    console.log(value)
+    if(isNaN(value)){
+        let  notNum=['J','Q','K']
+        let getting10=notNum.some((el)=>{
+            return value===el
+        })
+        if(getting10){
+           return 10
+        }else if(value==='A'){
+            return 11
+        }
+    }
+    return parseInt(value)
+    
 }
 
 
