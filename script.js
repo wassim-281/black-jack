df7bda75b0ce21f644e4c81f49d719d98064686b
wassim zisous <sissimodidoud@gmail.com>
2022-12-26 00:12:19 +0100

    creating and shufling the cards

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 let deck=[]
 
 
+creatTHCards()
+shufelTheCards()
+console.log(deck)
 function creatTHCards(){
     let value=['A','2','3','4','5','6','7','8','9','10','J','Q','K']
     let type=['D','H','S','C']
+    for(let i=0;i<value.length;i++){
+        for(let j=0;j<type.length;j++){
+            let card=`${value[i]}-${type[j]}`
+            deck.push(card)
+        }
+    }
 }
+
+function shufelTheCards(){
+    
+    
+    for(let i=0;i<deck.length;i++){
+        let random=Math.floor(Math.random()*deck.length)
+        let place=deck[i]
+        deck[i]=deck[random]
+        deck[random]=place
+    }
+}
+
+
